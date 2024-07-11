ff7210bfd4d253754f8cda637b1e15f82fdfd3cb
jietw <jiewang.tw@gmail.com>
2024-07-11 16:52:51 +0800

    add Post card component

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const AddPost = () => {
   return (
-    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
       <Image
         src="https://images.pexels.com/photos/13975586/pexels-photo-13975586.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
---
+++ src/components/Post.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Image from "next/image";
+
+const Post = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* USER */}
+      <div className="flex item-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/23476915/pexels-photo-23476915.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="font-medium">Jack McBride</span>
+        </div>
+        <Image
+          src="/more.png"
+          width={16}
+          height={16}
+          alt=""
+          className="w-6 h-6"
+        />
+      </div>
+      {/* DESC */}
+      <div className="flex flex-col gap-4">
+        <div className="w-full min-h-96 relative">
+          <Image
+            src="https://images.pexels.com/photos/26890891/pexels-photo-26890891.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            fill
+            alt=""
+            className="object-cover rounded-md"
+          />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
+          sint illum, commodi consequatur culpa dolore at? Exercitationem, quos?
+          Doloremque esse magnam, nam doloribus harum consectetur aliquid cumque
+          sed eos fugit.
+        </p>
+      </div>
+      {/* INTERACTION */}
+      <div className="flex items-center justify-between text-sm my-4">
+        <div className="flex gap-8">
+          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+            <Image
+              src="/like.png"
+              width={16}
+              height={16}
+              alt=""
+              className="w-6 h-6 cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123<span className="hidden md:inline"> Likes</span>
+            </span>
+          </div>
+          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+            <Image
+              src="/comment.png"
+              width={16}
+              height={16}
+              alt=""
+              className="w-6 h-6 cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123<span className="hidden md:inline"> Likes</span>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+            <Image
+              src="/share.png"
+              width={16}
+              height={16}
+              alt=""
+              className="w-6 h-6 cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123<span className="hidden md:inline"> Likes</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
