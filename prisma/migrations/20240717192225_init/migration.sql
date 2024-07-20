8531fc4a751c1f857853a5d66449cf115af96d4f
jietw <jiewang.tw@gmail.com>
2024-07-20 11:12:12 +0800

    design Prisma ORM db

---
+++ prisma/migrations/20240717192225_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Test` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` VARCHAR(191) NULL,
+    `number` INTEGER NOT NULL,
+    `isPublished` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
